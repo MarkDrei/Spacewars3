75f1cab6a55a85eccb733b1fef1dbd3067077b94
Mark <mark@mopedoversum.de>
2025-06-08 00:11:41 +0200

    Removed unused code

--- src/Asteroid.ts
+++ src/Asteroid.ts
@@ -6,43 +6,4 @@ export class Asteroid extends SpaceObject {
         const angleRadians = ((angleDegrees - 90) * Math.PI) / 180;
         super(x, y, angleRadians, speed);
     }
-
-    getX(): number {
-        return this.x;
-    }
-
-    getY(): number {
-        return this.y;
-    }
-
-    getAngle(): number {
-        return this.angle;
-    }
-
-    updatePosition(deltaTime: number): void {
-        if (this.speed === 0) return; // Don't update position if speed is 0
-        this.x += this.speed * Math.cos(this.angle) * deltaTime;
-        this.y += this.speed * Math.sin(this.angle) * deltaTime;
-    }
-
-    draw(ctx: CanvasRenderingContext2D, centerX: number, centerY: number, shipX: number, shipY: number): void {
-        const asteroidScreenX = centerX + this.x - shipX;
-        const asteroidScreenY = centerY + this.y - shipY;
-        ctx.save();
-        ctx.translate(asteroidScreenX, asteroidScreenY);
-        // Add 90 degrees (Math.PI/2) to align the visual orientation with movement direction
-        ctx.rotate(this.angle + Math.PI/2);
-        // Draw a simple triangle pointing upwards with a sharper angle
-        ctx.beginPath();
-        ctx.moveTo(0, -10);    // top
-        ctx.lineTo(5, 5);      // bottom right
-        ctx.lineTo(-5, 5);     // bottom left
-        ctx.closePath();
-        ctx.fillStyle = '#ff5722'; // orange fill
-        ctx.fill();
-        ctx.strokeStyle = '#bf360c'; // darker orange stroke
-        ctx.lineWidth = 1;
-        ctx.stroke();
-        ctx.restore();
-    }
 } 
